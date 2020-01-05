7891d79a5e4b6722ccc726450fcd32a8095b520e
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-01-05 18:35:11 -0300

    lint e adição de suporte a rotas protegidas

--- test/utils/insert.js
+++ test/utils/insert.js
@@ -1,8 +1,11 @@
 const request = require('supertest');
 
-module.exports = (app) => async (table, data) => {
-  const result = (await request(app)
-    .post(`/${table}`)
-    .send(data)).body[0];
+module.exports = (app) => async (table, data, token = '') => {
+  const result = (
+    await request(app)
+      .post(`/${table}`)
+      .set('Authorization', token)
+      .send(data)
+  ).body[0];
   return result;
 };
