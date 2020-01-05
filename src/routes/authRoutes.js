d7b0a9ccf9cdfedb1f8ee4c895a0805f94be0146
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-01-05 18:27:12 -0300

    refatoração para nova estrutura

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,8 +3,6 @@ const AuthController = require('../controllers/authController.js');
 
 const router = express.Router();
 
-module.exports = (app) => {
-  router.post('/login', AuthController.login);
+router.post('/login', AuthController.login);
 
-  app.use(router);
-};
+module.exports = router;
