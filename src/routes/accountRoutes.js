935b6cf73ba5f63f6a0bbba375876ad2bc0f6dee
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-01-05 18:31:42 -0300

    adição de proteção de rotas

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
 const AccountController = require('../controllers/accountController');
 
 const router = express.Router();
 
-module.exports = (app) => {
-  router.get('/accounts', AccountController.index);
-  router.get('/accounts/:id', AccountController.show);
-  router.post('/accounts', AccountController.store);
-  router.put('/accounts/:id', AccountController.update);
-  router.delete('/accounts/:id', AccountController.destroy);
+router.use('/accounts', authMiddleware);
+router.get('/accounts', AccountController.index);
+router.get('/accounts/:id', AccountController.show);
+router.post('/accounts', AccountController.store);
+router.put('/accounts/:id', AccountController.update);
+router.delete('/accounts/:id', AccountController.destroy);
 
-  app.use(router);
-};
+module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
 const UserController = require('../controllers/userController');
 
 const router = express.Router();
 
-module.exports = (app) => {
-  router.get('/users', UserController.index);
-  router.post('/users', UserController.store);
+router.get('/users', authMiddleware, UserController.index);
+router.post('/users', UserController.store);
 
-  app.use(router);
-};
+module.exports = router;
